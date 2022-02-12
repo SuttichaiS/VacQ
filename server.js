b6190a4e03647d6f6bde8176b5f86b590462fefe
unknown <suriyasat.suttichai@gmail.com>
2022-02-12 14:26:00 +0700

    Connect with MongoDB Atlas

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
+//Load env vars
 dotenv.config({path:'./config/config.env'})
+
+//Connect to DB
+connectDB();
+
+//Route files
+const hospitals = require('./routes/hospitals.js');
 
 const app = express();
 
-const hospitals = require('./routes/hospitals.js');
+
+//Body parser
+app.use(express.json());
+
 app.use('/api/v1/hospitals', hospitals)
 
+// app.get('/', (req,res) => {
+//     res.status(200).json({success:true})
+// })
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 'mode on port ', PORT))
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 'mode on port ', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err,promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(()=> process.exit(1));
+}
+);
